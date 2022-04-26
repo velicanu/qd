b4409235f9c185ddbec0e2c2fbcd0fceabb53647
Dragos Velicanu <dragosvelicanu@gmail.com>
2022-04-25 22:06:50 -0400

    add tests

--- tests/qd_test.py
+++ tests/qd_test.py
@@ -1,8 +1,16 @@
 import json
 import os
+import subprocess
 import tempfile
 
+import pytest
 from qd import get_df
+
+
+@pytest.fixture
+def tmpfile():
+    with tempfile.TemporaryDirectory() as _tmpdir:
+        yield os.path.join(_tmpdir, "file.png")
 
 
 def test_get_df_json():
@@ -44,3 +52,80 @@ x,cos,sin
     ]
 
     assert actual == expected
+
+
+def test_cli_no_args():
+    proc = subprocess.run("./qd/qd.py -i data/cossin.json".split())
+    assert proc.returncode == 0
+
+
+def test_cli_line(tmpfile):
+    subprocess.run(f"./qd/qd.py -i data/cossin.json -y cos,sin -o {tmpfile}".split())
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_mean(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --mean -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_quant(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --quant -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_line_dualy(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --dualy -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_mean_dualy(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --mean --dualy -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_quant_dualy(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --quant --dualy -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_quant_75(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --quant -q 75 -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_mean_nbins(tmpfile):
+    subprocess.run(
+        f"./qd/qd.py -i data/cossin.json -y cos,sin --mean --nbins 9 -o {tmpfile}".split()
+    )
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_xcol(tmpfile):
+    subprocess.run(f"./qd/qd.py -i data/cossin.json -x cos -y sin -o {tmpfile}".split())
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_stdin(tmpfile):
+    cat = subprocess.Popen(("cat", "data/cossin.json"), stdout=subprocess.PIPE)
+    subprocess.check_output(("./qd/qd.py", "-o", tmpfile), stdin=cat.stdout)
+    assert os.path.exists(tmpfile)
+
+
+def test_cli_stdin_stdout(tmpfile):
+    cat = subprocess.Popen(("cat", "data/cossin.json"), stdout=subprocess.PIPE)
+    with open(tmpfile, "w") as f:
+        subprocess.Popen("./qd/qd.py", stdin=cat.stdout, stdout=f)
+    assert os.path.exists(tmpfile)
